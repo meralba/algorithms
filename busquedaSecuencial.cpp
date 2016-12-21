13e11afe7b218d5a4aad9458f26a556d55202c98
Mer Alba <MercAddons@users.noreply.github.com>
2016-12-21 13:12:03 +0100

    resolves #2

--- busquedaSecuencial.cpp
+++ busquedaSecuencial.cpp
@@ -1,15 +1,23 @@
-// Example program
+/** Algoritmo de búsqueda secuencial
+  * @author Mer Alba
+  */
 #include <iostream>
 
 using namespace std;
 
+/** Algoritmo de búsqueda secuencial de un elemento "dato" en un vector "n"
+  * @param n puntero al vector en el que se busca
+  * @param tam tamaño del vector
+  * @param dato Elemento que vamos a buscar en el vector
+  * @return indice Devuelve el índice dónde se ha encontrado "dato"
+  */
 int busquedaSecuencial(int *n, int tam, int dato){
     bool encontrado=false;
     int indice=-1;
-    for(int i=0; i<tam && !encontrado; i++){
-        if(n[i]==dato){
-            encontrado=true;
-            indice=i;
+    for(int i=0; i<tam && !encontrado; i++){    //Se recorre el vector hasta que se encuentra el elemento o hasta el final
+        if(n[i]==dato){     // Si el elemento de esta iteración es el elemento que se está buscando 
+            encontrado=true;    // Se pone a true la variable booleana que controla si se ha encontrado o no...
+            indice=i;           // Se actualiza el índice
         }
     }
     return indice;
